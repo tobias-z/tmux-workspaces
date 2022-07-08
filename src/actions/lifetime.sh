987b54503ac137d8d572053aeb48cec93f0fe799
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-07-08 17:53:39 +0100

    feat: allow running start, stop and restart on single windows

---
+++ src/actions/lifetime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+action=$1
+specific_window=$2
+selected_name=$(tmux display-message -p "#S")
+path=$(dirname $(realpath $0))
+
+if [ -n "$specific_window" ]; then
+    script="$TMUX_WORKSPACES/$selected_name/$specific_window/$action"
+    dir_name=$(dirname $0)
+
+    if [ -e "$script" ]; then
+        while read line; do
+            sh $path/../windows/key-sender.sh $specific_window $selected_name "$line"
+        done < $script
+    fi
+else
+    sh $path/../windows/all-runner.sh $(tmux display-message -p "#S") $action
+fi

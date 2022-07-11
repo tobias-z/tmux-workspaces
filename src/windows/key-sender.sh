20b4cb192dc961dd02ca91926a5de4c4efd4c24f
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-11 12:30:07 +0200

    fix shifting issue on main window

--- src/windows/key-sender.sh
+++ src/windows/key-sender.sh
@@ -2,15 +2,21 @@
 
 branch_name=$(basename $1)
 session_name=$2
-selected_path=$3
 clean_name=$(echo $branch_name | tr "./" "__")
 target="$session_name:$clean_name"
 
 if ! tmux has-session -t $target 2> /dev/null; then
+    selected_path=$3
     tmux neww -dn $clean_name -t $session_name -c "$selected_path"
+    shift
     sleep 1
 fi
 
-shift 3
+shift 2
+
+if [ "$branch_name" = "$TW_MAIN_WINDOW" ]; then
+    shift
+fi
+
 tmux send-keys -t $target C-c C-d
 tmux send-keys -t $target "$*" Enter
